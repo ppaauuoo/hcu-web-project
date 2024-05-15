55a691fe8f5cf2ec604d443c638aaefa9e1d8c96
sittikornnn <sittikorn720@gmail.com>
2024-05-15 13:04:18 +0700

    update Nav image

--- frontend/src/components/RootLayout.tsx
+++ frontend/src/components/RootLayout.tsx
@@ -1,29 +1,17 @@
+
 import {
-  Activity,
-  ArrowUpRight,
+
   CircleUser,
-  CreditCard,
-  DollarSign,
+
   Menu,
   Package2,
   Search,
-  Users,
+
 } from "lucide-react"
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
-import { Badge } from "@/components/ui/badge"
+
 import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -34,14 +22,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
+
 import { Link } from "react-router-dom"
 
 import { Outlet } from "react-router-dom";
@@ -49,13 +30,19 @@ import { Outlet } from "react-router-dom";
 export default function RootLayout(){
   return (
   <>
-  <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-  <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+  
+  <header className="sticky top-0 flex h-16 items-center gap-30 border-b bg-background px-4 md:px-6">
+  <img className="h-10 w-10 m-1" src="/images.jpg" alt="image description"/>
+
+  {/* navbar */}
+  <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-4 md:text-sm lg:gap-6">
           <Link
-            to="#"
+            to="index.tsx"
             className="flex items-center gap-2 text-lg font-semibold md:text-base"
           >
-            <Package2 className="h-6 w-6" />
+            {/* <Package2 className="h-6 w-6" /> */}
+            
+            
             <span className="sr-only">Acme Inc</span>
           </Link>
           <Link
@@ -92,7 +79,7 @@ export default function RootLayout(){
             to="#"
             className="text-muted-foreground transition-colors hover:text-foreground"
           >
-            บริการสิชาการ
+            บริการวิชาการ
           </Link>
           <Link
             to="#"
@@ -113,6 +100,8 @@ export default function RootLayout(){
             ติดต่อเรา
           </Link>
         </nav>
+
+        {/* side menu */}
         <Sheet>
           <SheetTrigger asChild>
             <Button
@@ -163,6 +152,8 @@ export default function RootLayout(){
             </nav>
           </SheetContent>
         </Sheet>
+
+        {/* search bar*/}
         <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
           <form className="ml-auto flex-1 sm:flex-initial">
             <div className="relative">
@@ -174,6 +165,8 @@ export default function RootLayout(){
               />
             </div>
           </form>
+
+          {/*  account menu  */}
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="secondary" size="icon" className="rounded-full">
