d0d47fc9370d115d8f5e553cd04e35ca496c5b75
sittikornnn <115983121+sittikornnn@users.noreply.github.com>
2024-05-20 09:07:51 +0700

    Add files via upload

--- frontend/src/components/RootLayout.tsx
+++ frontend/src/components/RootLayout.tsx
@@ -1,8 +1,149 @@
-import { Outlet } from "react-router-dom";
 
-export default function RootLayout(){
+import {
+  Menu,
+} from "lucide-react"
+
+
+import { Button } from "@/components/ui/button"
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+
+import { Link } from "react-router-dom"
+
+export default function RootLayout() {
   return (
-  <>
-    <Outlet/>
-  </>)
+    <>
+
+      <header className="sticky top-0 flex justify-between md:justify-start h-16 items-center gap-30 border-b bg-background px-4 md:px-6">
+        <img className="h-10 w-10 m-1" src="/images.jpg" alt="image description" />
+        <div className="flex justify-center">
+          <h1 className="lg:hidden md:hidden sm:not-sr-only flex justify-end">คณะวิทยาศาสตร์และเทคโนโลยี<br/>มหาวิทยาลัยหัวเฉียวเฉลิมพระเกียรติ</h1>
+        </div>
+        {/* navbar */}
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-4 md:text-sm lg:gap-6">
+          <Link
+            to="index.tsx"
+            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+          >
+            <span className="sr-only">หน้าแรก</span>
+          </Link>
+          <Link
+            to="#"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            เกี่ยวกับเรา
+          </Link>
+          <Link
+            to="#"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            หลักสูตร
+          </Link>
+          <Link
+            to="#"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            การเรียนการสอน
+          </Link>
+          <Link
+            to="#"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            การวิจัย
+          </Link>
+          <Link
+            to="#"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            บริการวิชาการ
+          </Link>
+          <Link
+            to="#"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            กิจการนักศึกษา
+          </Link>
+          <Link
+            to="#"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            กิจการนักศึกษา
+          </Link>
+          <Link
+            to="#"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            ติดต่อเรา
+          </Link>
+        </nav>
+
+        {/* side menu */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right">
+            <nav className="grid gap-6 text-lg font-mediu">
+              <Link
+                to="#"
+                className="text-muted-foreground transition-colors hover:text-foreground"
+              >
+                เกี่ยวกับเรา
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground transition-colors hover:text-foreground"
+              >
+                หลักสูตร
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground transition-colors hover:text-foreground"
+              >
+                การเรียนการสอน
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground transition-colors hover:text-foreground"
+              >
+                การวิจัย
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground transition-colors hover:text-foreground"
+              >
+                บริการวิชาการ
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground transition-colors hover:text-foreground"
+              >
+                กิจการนักศึกษา
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground transition-colors hover:text-foreground"
+              >
+                กิจการนักศึกษา
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground transition-colors hover:text-foreground"
+              >
+                ติดต่อเรา
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </header>
+
+      {/* <Outlet/> */}
+    </>)
 }
