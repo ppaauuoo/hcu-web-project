30685fe3810d2d2ad2fef1650a3cef6bd467ca08
sittikornnn <sittikorn720@gmail.com>
2024-05-28 10:12:30 +0700

    trans, white text navbar

--- frontend/src/components/RootLayout.tsx
+++ frontend/src/components/RootLayout.tsx
@@ -8,70 +8,72 @@ import { Button } from "@/components/ui/button"
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 
-import { Link } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 export default function RootLayout() {
   return (
     <>
 
-      <header className="sticky top-0 flex justify-between md:justify-start h-16 items-center gap-30 border-b bg-background px-4 md:px-6">
-        <img className="h-10 w-10 m-1" src="/images.jpg" alt="image description" />
-        <div className="flex justify-center">
-          <h1 className="lg:hidden md:hidden sm:not-sr-only flex justify-end">คณะวิทยาศาสตร์และเทคโนโลยี<br/>มหาวิทยาลัยหัวเฉียวเฉลิมพระเกียรติ</h1>
-        </div>
+      <header className="sticky top-0 flex justify-between md:justify-center h-16 items-center gap-30 bg-transparent px-4 md:px-6 ">
+        <img className="h-10 w-10 m-1 md:mr-20 lg:mr-40" src="/images.jpg" alt="image description" />
+        {/* mobile nav */}
+        <nav className="flex justify-center">
+          <h1 className="text-muted-foreground text-white lg:hidden md:hidden sm:not-sr-only flex justify-end">คณะวิทยาศาสตร์และเทคโนโลยี<br />มหาวิทยาลัยหัวเฉียวเฉลิมพระเกียรติ</h1>
+        </nav>
         {/* navbar */}
-        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-4 md:text-sm lg:gap-6">
+        <nav className="hidden flex-col gap-6 text-lgfont-medium md:flex md:flex-row md:items-center md:gap-4 md:text-sm lg:gap-6">
+
           <Link
             to="index.tsx"
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+            className="flex  items-center gap-2 text-lg font-semibold md:text-base"
           >
             <span className="sr-only">หน้าแรก</span>
           </Link>
           <Link
             to="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
+            className="text-muted-foreground text-white transition-colors hover:text-foreground"
           >
             เกี่ยวกับเรา
           </Link>
           <Link
             to="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
+            className="text-muted-foreground text-white transition-colors hover:text-foreground"
           >
             หลักสูตร
           </Link>
           <Link
             to="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
+            className="text-muted-foreground text-white transition-colors hover:text-foreground"
           >
             การเรียนการสอน
           </Link>
           <Link
             to="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
+            className="text-muted-foreground text-white transition-colors hover:text-foreground"
           >
             การวิจัย
           </Link>
           <Link
             to="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
+            className="text-muted-foreground text-white transition-colors hover:text-foreground"
           >
             บริการวิชาการ
           </Link>
           <Link
             to="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
+            className="text-muted-foreground text-white transition-colors hover:text-foreground"
           >
             กิจการนักศึกษา
           </Link>
           <Link
             to="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
+            className="text-muted-foreground text-white transition-colors hover:text-foreground"
           >
             กิจการนักศึกษา
           </Link>
           <Link
             to="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
+            className="text-muted-foreground text-white transition-colors hover:text-foreground"
           >
             ติดต่อเรา
           </Link>
@@ -144,6 +146,6 @@ export default function RootLayout() {
         </Sheet>
       </header>
 
-      {/* <Outlet/> */}
+      <Outlet />
     </>)
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -46,9 +46,16 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className=" min-h-screen w-full flex-col">
       
+      <div>
+        <img className="" src="/hero.png" />
+      </div>
+
+
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+
+
         <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
           <Card x-chunk="dashboard-01-chunk-0">
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
