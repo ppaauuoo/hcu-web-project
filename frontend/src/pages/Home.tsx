206f00aff8dd64d796325e12a580b099a1f8c248
ppaauuoo <bcg.channel@gmail.com>
2024-05-29 09:26:09 +0700

    feat(home): cleaning import

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,8 @@
 import {
   Activity,
   ArrowUpRight,
-  CircleUser,
   CreditCard,
   DollarSign,
-  Menu,
-  Package2,
-  Search,
   Users,
 } from "lucide-react";
 
@@ -20,16 +16,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
 import {
   Table,
   TableBody,
