8ca062465a4e9769e052cd9086db541168ddf8c7
sittikornnn <sittikorn720@gmail.com>
2024-05-31 13:09:31 +0700

    fix!(runtime): remove millionjs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,12 @@
-import MillionLint from '@million/lint';
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-const plugins = [react()];
-plugins.unshift(MillionLint.vite())
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
 export default defineConfig({
-  plugins: plugins,
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-});
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
